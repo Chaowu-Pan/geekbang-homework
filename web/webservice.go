6d590e763c5360303a9f0f9588e39b2e3702f80a
panchaowu <chaowu.pan@qq.com>
2021-05-09 21:49:14 +0800

    homework/week02

--- web/webservice.go
+++ web/webservice.go
@@ -19,21 +19,26 @@ func main() {
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello World!")
 	})
+
+	//应用服务
 	serverApp := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: mux,
 	}
 
+	//监控服务
 	serverDebug := &http.Server{
 		Addr: "127.0.0.1:8081",
 	}
 
 	servers := []*http.Server{serverApp, serverDebug}
 
+	//启动应用服务
 	group.Go(func() error {
 		return serverApp.ListenAndServe()
 	})
 
+	//启动监控服务
 	group.Go(func() error {
 		return serverDebug.ListenAndServe()
 	})
@@ -44,8 +49,10 @@ func main() {
 
 		select {
 		case <-c:
+			//接收到系统信号
 			shutdownServers(servers)
 		case <-ctx.Done():
+			//有服务返回错误
 			shutdownServers(servers)
 		}
 		log.Println("server gracefully shutdown")
@@ -59,6 +66,7 @@ func main() {
 
 }
 
+//关闭所有服务
 func shutdownServers(servers []*http.Server) {
 	for _, server := range servers {
 		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
